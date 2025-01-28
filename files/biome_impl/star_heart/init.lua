9d7e189291cc2c86b2e53703430ca7e81125e1ed
Reidelrick <alexis.leviavant@gmail.com>
2025-01-28 17:13:07 +0100

    Materials + Items + Extensions Shenanigans

--- files/biome_impl/star_heart/init.lua
+++
@@ -1,3 +0,0 @@
-local entity = GetUpdatedEntityID();
-local x, y = EntityGetTransform( entity );
-EntityLoad( "mods/law-of-attraction/files/entities/items/pickup/startstone.xml", x + 256 , y + 256 );
--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 dofile_once( "data/scripts/lib/utilities.lua" )
+dofile("mods/law-of-attraction/api_def.lua")
+
+
+function OnPlayerSpawned( player_entity )
+	EntityLoad('mods/law-of-attraction/files/biome_impl/star_nest/star_nest.xml', 6650, 14881 );
+end
 
 -- This code runs when all mods' filesystems are registered
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/law-of-attraction/files/scripts/gun/gun_actions.lua" )
@@ -10,3 +16,4 @@ ModLuaFileAppend( "data/scripts/items/potion.lua", "mods/law-of-attraction/files
 -- Custom Status support injection
 ModLuaFileAppend("data/scripts/status_effects/status_list.lua",
 	"mods/law-of-attraction/files/scripts/status_effects/status_effects.lua")
+
